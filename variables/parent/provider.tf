8a642882cb8b34d435ccc6c80e9e944bf104d8fc
Alvina88934 <alvinaadnan88@gmail.com>
2025-09-22 16:50:14 +0100

    new code added

--- variables/parent/provider.tf
+++ variables/parent/provider.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.27.0"
+      version = "4.44.0"
     }
   }
   backend "azurerm" {
-   resource_group_name = "bagrg3-resources"
-    storage_account_name = "bagstorage24"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "bagcontainer"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+   resource_group_name = " Rg_123"
+    storage_account_name = "azstorage789"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "azcontainer789"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "variable.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "d61b0c96-122d-4292-bdc9-546f9ea90ccd"
+  subscription_id = "aa50dcb0-04de-41e4-bb3e-260e54d18bec"
     
   }
